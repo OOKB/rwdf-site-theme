fc30b47a441bbc011370220809cee8116b00b9bd
Kristian Bjørnard <kristian.bjornard@gmail.com>
2014-04-17 18:39:30 -0400

    cleaning up JS

--- scripts/rwdf.js
+++ scripts/rwdf.js
@@ -12,36 +12,37 @@ $(document).ready(function() {
 
   //prepare whatwedo feed for isotope...
   $('.icons.categories .science a').attr('href', '#filter=.science');
-  $('.icons.categories a.science').attr('href', '/whatwedo/#filter=.science');
   $('.icons.categories .scholars a').attr('href', '#filter=.scholars');
-  $('.icons.categories a.scholars').attr('href', '/whatwedo/#filter=.scholars');
   $('.icons.categories .arts a').attr('href', '#filter=.arts');
-  $('.icons.categories a.arts').attr('href', '/whatwedo/#filter=.arts');
   $('.icons.categories .justice a').attr('href', '#filter=.justice');
-  $('.icons.categories a.justice').attr('href', '/whatwedo/#filter=.justice');
   $('.icons.categories .community a').attr('href', '#filter=.community');
+
+  $('.icons.categories a.science').attr('href', '/whatwedo/#filter=.science');
+  $('.icons.categories a.scholars').attr('href', '/whatwedo/#filter=.scholars');
+  $('.icons.categories a.arts').attr('href', '/whatwedo/#filter=.arts');
+  $('.icons.categories a.justice').attr('href', '/whatwedo/#filter=.justice');
   $('.icons.categories a.community').attr('href', '/whatwedo/#filter=.community');
 
   var $container = $('#projects');
   $container.isotope({ layoutMode : 'fitRows' });
 
   $('#filters a').click(function(){
-        // get href attr, remove leading #
+    // get href attr, remove leading #
     var href = $(this).attr('href').replace( /^#/, '' ),
-        // convert href into object
-        // i.e. 'filter=.inner-transition' -> { filter: '.inner-transition' }
-        option = $.deparam( href, true );
+    // convert href into object
+    // i.e. 'filter=.inner-transition' -> { filter: '.inner-transition' }
+    option = $.deparam( href, true );
     // set hash, triggers hashchange on window
     $.bbq.pushState( option );
     return false;
   });
 
   $('#projects .icons.categories a').click(function(){
-        // get href attr, remove leading #
+    // get href attr, remove leading #
     var href = $(this).attr('href').replace( /^#/, '' ),
-        // convert href into object
-        // i.e. 'filter=.inner-transition' -> { filter: '.inner-transition' }
-        option = $.deparam( href, true );
+    // convert href into object
+    // i.e. 'filter=.inner-transition' -> { filter: '.inner-transition' }
+    option = $.deparam( href, true );
     // set hash, triggers hashchange on window
     $.bbq.pushState( option );
     return false;
@@ -53,8 +54,8 @@ $(document).ready(function() {
     // apply options from hash
     $container.isotope( hashOptions );
   })
-    // trigger hashchange to capture any hash data on init
-    .trigger('hashchange');
+  // trigger hashchange to capture any hash data on init
+  .trigger('hashchange');
 
 });
 
