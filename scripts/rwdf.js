67f21092cb0d22b36e6eb2934db27825e449402b
Kristian Bjørnard <kristian.bjornard@gmail.com>
2014-04-17 18:29:58 -0400

    updates to some JS

--- scripts/rwdf.js
+++ scripts/rwdf.js
@@ -10,52 +10,6 @@ $(document).ready(function() {
     $('#slideshow .slide').css('height','100%');
   }
 
-  //prepare whatwedo feed for isotope...
-  $('.icons.categories .science a').attr('href', '#filter=.science');
-  $('.icons.categories a.science').attr('href', '/whatwedo/#filter=.science');
-  $('.icons.categories .scholars a').attr('href', '#filter=.scholars');
-  $('.icons.categories a.scholars').attr('href', '/whatwedo/#filter=.scholars');
-  $('.icons.categories .arts a').attr('href', '#filter=.arts');
-  $('.icons.categories a.arts').attr('href', '/whatwedo/#filter=.arts');
-  $('.icons.categories .justice a').attr('href', '#filter=.justice');
-  $('.icons.categories a.justice').attr('href', '/whatwedo/#filter=.justice');
-  $('.icons.categories .community a').attr('href', '#filter=.community');
-  $('.icons.categories a.community').attr('href', '/whatwedo/#filter=.community');
-
-  var $container = $('#projects');
-  $container.isotope({ layoutMode : 'fitRows' });
-
-  $('#filters a').click(function(){
-        // get href attr, remove leading #
-    var href = $(this).attr('href').replace( /^#/, '' ),
-        // convert href into object
-        // i.e. 'filter=.inner-transition' -> { filter: '.inner-transition' }
-        option = $.deparam( href, true );
-    // set hash, triggers hashchange on window
-    $.bbq.pushState( option );
-    return false;
-  });
-
-  $('#projects .icons.categories a').click(function(){
-        // get href attr, remove leading #
-    var href = $(this).attr('href').replace( /^#/, '' ),
-        // convert href into object
-        // i.e. 'filter=.inner-transition' -> { filter: '.inner-transition' }
-        option = $.deparam( href, true );
-    // set hash, triggers hashchange on window
-    $.bbq.pushState( option );
-    return false;
-  });
-
-  $(window).bind( 'hashchange', function( event ){
-    // get options object from hash
-    var hashOptions = $.deparam.fragment();
-    // apply options from hash
-    $container.isotope( hashOptions );
-  })
-    // trigger hashchange to capture any hash data on init
-    .trigger('hashchange');
-
 });
 
 $(window).resize(function() {
@@ -82,12 +36,6 @@ $(window).bind("load", function() {
   
   $('.spruceuptabs .spruceuplist ul li').first().addClass('active');
   $('.spruceuptabs .tab-content > div.tab-pane').first().addClass('active');
-});
-
-//Working on making sidebar navigation follow active trail.
-$("#filters a").click(function() {
-  $("#filters").find('li.active').removeClass('active');
-  $(this).parent().parent().addClass('active');
 });
 
 // FancyBox
--- scripts/whoweare.js
+++ scripts/whoweare.js
@@ -1,13 +1,16 @@
 // why is this doing this?
 // why are you not properly listening!
 
-// store the currently selected tab in the hash value
-$("ul.nav-tabs > li > a").on("shown.bs.tab", function (e) {
-    var id = $(e.target).attr("href").substr(1);
-    window.location.hash = id;
+$(document).ready(function() {
+  
+  // store the currently selected tab in the hash value
+  $("ul.nav-tabs > li > a").on("shown.bs.tab", function (e) {
+      var id = $(e.target).attr("href").substr(1);
+      window.location.hash = id;
+  });
+  
+  // on load of the page: switch to the currently selected tab
+  var hash = window.location.hash;
+  $('#myTab a[href="' + hash + '"]').tab('show');
+  
 });
-
-// on load of the page: switch to the currently selected tab
-var hash = window.location.hash;
-$('#myTab a[href="' + hash + '"]').tab('show');
-
