4db90c3238c71767587a2a6b708c6f39efc39457
Kai Curry <kai@sundaysenergy.com>
2014-04-24 09:04:02 -0500

    admin js one more time

--- scripts/admin.js
+++ scripts/admin.js
@@ -5,7 +5,9 @@ window.log = function() {
 $(document).ready(function() {
   var endpoint = '/whatwedo/projects.json';
   $.getJSON(endpoint, function(data) {
+    // Assign template from html. Script tag with id.
     var template = Hogan.compile($('#projects-template').html());
+    // Render and add to html in one step.
     $('div#projects').append(template.render(data));
   });
 });
