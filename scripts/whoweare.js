9e8057df1e8159b2afe52c7e79c4a011d15e682c
Kristian Bjørnard <kristian.bjornard@gmail.com>
2014-04-29 21:49:44 -0400

    theme JS isn't updating?

--- scripts/whoweare.js
+++ scripts/whoweare.js
@@ -6,14 +6,6 @@ $("#myTab li a").on("click", function(e) {
   history.pushState({}, "", this.href);
 });
 
-// store the currently selected tab in the hash value
-/*
-$("#myTab li a").on("shown.bs.tab", function (e) {
-  var id = $(e.target).attr("href").substr(1);
-  window.location.hash = id;
-});
-*/
-
 // on load of the page: switch to the currently selected tab
 var hash = window.location.hash;
 $('#myTab li a[href="' + hash + '"]').tab('show');
