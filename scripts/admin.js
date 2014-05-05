d13e01c3fbbf18b0f034757776145b674d52d8e4
Kai Curry <kai@sundaysenergy.com>
2014-05-05 12:12:51 -0500

    clean up admin page code a tiny bit.

--- scripts/admin.js
+++ scripts/admin.js
@@ -5,22 +5,6 @@ window.log = function() {
 $(document).ready(function() {
   var endpoint = '/whatwedo/projects.json';
   $.getJSON(endpoint, function(data) {
-    _.forEach(data.projects, function(project){
-      if (project.featureimg) {
-        // If string does not contain.
-        if (!~project.featureimg.indexOf('/')) {
-          project.featureimg = '/' + project._dirname + '/' + project.featureimg;
-        }
-      }
-      else {
-        if (project._image) {
-          project.featureimg = '/' + project._image._id;
-        }
-        else if (project._images) {
-          project.featureimg = '/' + project._images[0]._id;
-        }
-      }
-    });
     // Assign template from html. Script tag with id.
     var template = Hogan.compile($('#projects-template').html());
     // Render and add to html in one step.
