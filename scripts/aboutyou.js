bb667cd5783f25f17317076b0e87722232b2c0c5
Kai Curry <kai@sundaysenergy.com>
2014-05-09 21:38:42 -0500

    Issue #22 more. Use the jumbotron area...

--- scripts/aboutyou.js
+++ scripts/aboutyou.js
@@ -1,23 +1,30 @@
 $(document).ready(function() {
   var hash = window.location.hash;
-  var hash_html = $('div#thankyou_message');
+  var thanks_html = $('#thankyou_message');
+  var hed_html = $('#page_hed');
   var thanks_shown = false;
+
   var show_thanks = function() {
     hash = window.location.hash;
     if ('#thanks' == hash) {
       console.log('show thanks');
-      hash_html.show();
+      thanks_html.show();
+      hed_html.hide();
       thanks_shown = true;
     }
     else if (thanks_shown) {
       console.log('hide thanks');
-      hash_html.hide();
+      thanks_html.hide();
+      hed_html.show();
       thanks_shown = false;
     }
     else {
       console.log(hash);
     }
   }
+
   window.onhashchange = show_thanks;
+
   show_thanks();
+
 });
