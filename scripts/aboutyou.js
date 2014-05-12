7e8b1159f79dcbdaea09bfda707017f1176e3791
Kristian Bjørnard <kristian.bjornard@gmail.com>
2014-05-12 13:19:18 -0400

    tweaks to #thanks

--- scripts/aboutyou.js
+++ scripts/aboutyou.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   var hash = window.location.hash;
   var thanks_html = $('#thankyou_message');
   var hed_html = $('#page_hed');
+  var body_html = $('#aboutyou_form');
   var thanks_shown = false;
 
   var show_thanks = function() {
@@ -10,12 +11,14 @@ $(document).ready(function() {
       console.log('show thanks');
       thanks_html.show();
       hed_html.hide();
+      body_html.hide();
       thanks_shown = true;
     }
     else if (thanks_shown) {
       console.log('hide thanks');
       thanks_html.hide();
       hed_html.show();
+      body_html.show();
       thanks_shown = false;
     }
     else {
